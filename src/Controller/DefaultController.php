8759730eb8060c09578497c4baae7f669321c7dd
NEL45 <n2weickert@gmail.com>
2023-02-04 05:28:30 +0100

    controllers routes and twig

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'app_index')]
+class DefaultController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'welcome' => 'Bienvenue',
+        ]);
+    }
+}
